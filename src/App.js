818bf0685d51a99d0f7ef51fa86e5deb2e67a2ff
Shubham Pathak <shubham.pathak926@gmail.com>
2023-01-03 16:45:18 +0530

    basic html is done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,37 @@ import React from 'react'
 const App = () => {
   return (
     <>
-      
+      <div className='main-div'>
+          <div className='child-div'>
+              <figure>
+                <img src='' alt='todo-logo'/>
+                <figcaption>Add Your List Here...✌</figcaption>
+              </figure>
+                <div className='addItem'>
+                   <input type='text' placeholder='✍ Add Item' className='form-control'/>
+                   <i className='fa fa-plus add-btn' ></i>
+                </div>
+                    {/* Show Items Here */}
+                      <div className='showItems'>
+                         <div className='eachItem'>
+                            <div className='todo-btn'>
+
+                                  <h3>apple</h3>
+                               <i className='far fa-edit add-btn' ></i>
+                               <i className='fa fa-trash-alt add-btn' ></i>
+
+                             </div>
+
+                          </div>
+                      </div>
+
+                      {/* Remove Button */}
+                  <div className='showItems'>
+                   <button className='btn effect04' data-sm-link-text="REMOVE ALL"> <span> CHECK LIST</span> </button>
+
+                   </div>
+          </div>
+      </div> 
     </>
   )
 }
