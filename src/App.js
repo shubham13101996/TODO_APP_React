60c9d7f188f835a929724e24cfbcc000980e7d91
Shubham Pathak <shubham.pathak926@gmail.com>
2023-01-04 15:39:24 +0530

    App UI is done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import "./style.css"
 const App = () => {
   return (
     <>
@@ -10,29 +10,26 @@ const App = () => {
                 <figcaption>Add Your List Here...✌</figcaption>
               </figure>
                 <div className='addItem'>
-                   <input type='text' placeholder='✍ Add Item' className='form-control'/>
+                   <input type='text' placeholder=' ✍ Add Item..' className='form-control'/>
                    <i className='fa fa-plus add-btn' ></i>
                 </div>
-                    {/* Show Items Here */}
+                      {/* Show Items Here */}
+
                       <div className='showItems'>
                          <div className='eachItem'>
+                            <h3>apple</h3>
                             <div className='todo-btn'>
-
-                                  <h3>apple</h3>
                                <i className='far fa-edit add-btn' ></i>
-                               <i className='fa fa-trash-alt add-btn' ></i>
-
+                               <i className='far fa-trash-alt add-btn' ></i>
                              </div>
-
                           </div>
                       </div>
 
                       {/* Remove Button */}
-                  <div className='showItems'>
-                   <button className='btn effect04' data-sm-link-text="REMOVE ALL"> <span> CHECK LIST</span> </button>
-
-                   </div>
-          </div>
+                <div className='showItems'>
+                 <button className='btn effect04' data-sm-link-text="REMOVE ALL"> <span> CHECK LIST</span> </button>
+              </div>
+         </div>
       </div> 
     </>
   )
