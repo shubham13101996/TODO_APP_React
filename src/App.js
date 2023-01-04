0298c53d8dd6ad7dc9316c5d5ec1da637dd9cf59
Shubham Pathak <shubham.pathak926@gmail.com>
2023-01-04 16:03:12 +0530

    adding and showing items on UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./style.css"
 const App = () => {
+
+  const [input,setInput]=useState('');
+  const[item,setItem]=useState([]); 
+
+  const addItem = ()=>{
+    if(!input){
+      alert('Empty List Cannot Be Store..!!')
+    }else{
+    setItem([...item,input]);
+    setInput('');
+    }
+  }
+
   return (
     <>
       <div className='main-div'>
@@ -10,19 +23,26 @@ const App = () => {
                 <figcaption>Add Your List Here...✌</figcaption>
               </figure>
                 <div className='addItem'>
-                   <input type='text' placeholder=' ✍ Add Item..' className='form-control'/>
-                   <i className='fa fa-plus add-btn' ></i>
+                   <input type='text' placeholder=' ✍ Add Item..' className='form-control'
+                    value={input}
+                    onChange={(e)=>setInput(e.target.value)}
+                   />
+                   <i className='fa fa-plus add-btn' onClick={addItem}></i>
                 </div>
                       {/* Show Items Here */}
 
                       <div className='showItems'>
-                         <div className='eachItem'>
-                            <h3>apple</h3>
+                         {item.map((curElem,index)=>{
+                          return (
+                            <div className='eachItem' key={index}>
+                            <h3>{curElem}</h3>
                             <div className='todo-btn'>
                                <i className='far fa-edit add-btn' ></i>
                                <i className='far fa-trash-alt add-btn' ></i>
                              </div>
                           </div>
+                          )
+                          })}
                       </div>
 
                       {/* Remove Button */}
